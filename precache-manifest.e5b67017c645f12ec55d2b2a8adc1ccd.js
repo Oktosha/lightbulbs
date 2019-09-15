e63f6c4744085c8886e0a55003d7f0fd7637fff8
Daria Kolodzey <kolodaria@gmail.com>
2019-09-15 14:30:31 +0300

    Updates

---
+++ precache-manifest.e5b67017c645f12ec55d2b2a8adc1ccd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687dfc06338df5357c0a48be8eff373b",
+    "url": "/lightbulbs/index.html"
+  },
+  {
+    "revision": "b2582c61be8beea1ade3",
+    "url": "/lightbulbs/static/css/main.661ec596.chunk.css"
+  },
+  {
+    "revision": "0804007f636a85881813",
+    "url": "/lightbulbs/static/js/2.ab2baf33.chunk.js"
+  },
+  {
+    "revision": "b2582c61be8beea1ade3",
+    "url": "/lightbulbs/static/js/main.41d40653.chunk.js"
+  },
+  {
+    "revision": "7331b275dbdc95a5334b",
+    "url": "/lightbulbs/static/js/runtime~main.1b97642d.js"
+  },
+  {
+    "revision": "a2be0ba267b2eee1b23bf26fd641f1ff",
+    "url": "/lightbulbs/static/media/button-inactive.a2be0ba2.svg"
+  },
+  {
+    "revision": "f9c31452979709aa09f374058010f6a7",
+    "url": "/lightbulbs/static/media/button.f9c31452.svg"
+  },
+  {
+    "revision": "daf85581c7b4af7390094940d0b86f5c",
+    "url": "/lightbulbs/static/media/lightbulb-inactive.daf85581.svg"
+  },
+  {
+    "revision": "4620fecc986e5596850d6e941d07c28b",
+    "url": "/lightbulbs/static/media/lightbulb.4620fecc.svg"
+  }
+]);
